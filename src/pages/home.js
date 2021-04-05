775b1262886f0f0dd5a8c838fc77898041bdede7
Italo Marca <italomarca@icloud.com>
2021-04-05 12:07:34 -0300

    refactores the shit out of this app

---
+++ src/pages/home.js
@@ -0,0 +1,34 @@
+import {useEffect, useState} from 'react';
+
+
+import moviedbService from '../services/moviedbService'
+import Loading from '../components/Loading'
+import Header from '../components/Header'
+import MovieDescription from '../components/MovieDescription'
+
+
+const Home = () => {
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+    updateMovie()
+  }, [])
+
+  const updateMovie = async () => {
+    const movie = await moviedbService.getMovie()
+    setMovie(movie)
+  }
+  
+  return (
+    <>
+      <Header />
+      {
+        movie
+        ? <MovieDescription movie={movie} updateMovie={updateMovie} />
+        : <Loading />
+      }
+    </>
+  )
+}
+
+export default Home;
