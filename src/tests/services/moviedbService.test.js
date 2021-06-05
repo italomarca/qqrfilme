c4c9cd4a0bbcb2f02a95ac43d88d7a40fa00f015
Beatriz Scopel <scopelbeatriz@gmail.com>
2021-06-05 11:00:56 -0300

    adjustments on identation, variable names, test names and test logic

--- src/tests/services/moviedbService.test.js
+++ src/tests/services/moviedbService.test.js
@@ -6,11 +6,19 @@ import moviedbService from '../../services/moviedbService'
 
 describe('moviedbService', () => {
 
-  const mockSuccessResponse = {results: [{title: 'Godzilla vs. Kong', overview: 'Em uma época em que os monstros andam na Terra, a luta da humanidade por seu futuro coloca Godzilla e Kong em rota de colisão que verá as duas forças mais poderosas da natureza no planeta se confrontarem em uma batalha espetacular para as idades. Enquanto Monarch embarca em uma missão perigosa em terreno desconhecido e descobre pistas sobre as origens dos Titãs, uma conspiração humana ameaça tirar as criaturas, boas e más, da face da terra para sempre.', vote_average: 8.3, poster_path: '/klAIFewuqcyEmH1SMtR2XbMyqzM.jpg', release_date: '2021-03-24'}]};
+  const movieFetchResponse = {
+    results: [{
+      title: 'Godzilla vs. Kong', 
+      overview: 'Em uma época em que os monstros andam na Terra, a luta da humanidade por seu futuro coloca Godzilla e Kong em rota de colisão que verá as duas forças mais poderosas da natureza no planeta se confrontarem em uma batalha espetacular para as idades. Enquanto Monarch embarca em uma missão perigosa em terreno desconhecido e descobre pistas sobre as origens dos Titãs, uma conspiração humana ameaça tirar as criaturas, boas e más, da face da terra para sempre.', 
+      vote_average: 8.3, 
+      poster_path: '/klAIFewuqcyEmH1SMtR2XbMyqzM.jpg', 
+      release_date: '2021-03-24'
+    }]
+  };
 
   global.fetch = jest.fn(() => 
     Promise.resolve({
-      json: () => Promise.resolve(mockSuccessResponse),
+      json: () => Promise.resolve(movieFetchResponse),
     })
   );
   
@@ -18,23 +26,23 @@ describe('moviedbService', () => {
     fetch.mockClear();
   });
 
-  it('Calls correct URI fetch', async () => {
+  it('calls correct URI fetch', async () => {
     await moviedbService.getMovie()
     expect(global.fetch).toHaveBeenCalledWith(`${moviedbService.MOVIEDB_API_BASE_URI}/movie/popular?api_key=${moviedbService.API_KEY}&language=pt-BR&page=1`)
   });
 
-  it('Calls endpoint only once', async () => {
+  it('calls endpoint only once', async () => {
     await moviedbService.getMovie()
     expect(global.fetch).toHaveBeenCalledTimes(1)
   });
 
   it('getMovie function returns correct object', async () => {
     expect(await moviedbService.getMovie()).toMatchObject({
-      name: mockSuccessResponse.results[0].title,
-      description: mockSuccessResponse.results[0].overview,
-      stars: mockSuccessResponse.results[0].vote_average,
-      imageUri: mockSuccessResponse.results[0].poster_path ? `${moviedbService.POSTER_API_BASE_URI}${mockSuccessResponse.results[0].poster_path}` : null,
-      year: mockSuccessResponse.results[0].release_date.substring(0,4)
+      name: movieFetchResponse.results[0].title,
+      description: movieFetchResponse.results[0].overview,
+      stars: movieFetchResponse.results[0].vote_average,
+      imageUri: movieFetchResponse.results[0].poster_path,
+      year: movieFetchResponse.results[0].release_date.substring(0,4)
     })
   });
 
