5b3bef94d5df76f337c2164efd160355046949b6
Italo Marca <italomarca@icloud.com>
2021-06-12 11:41:44 -0300

    Adds navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import Home from './pages/home'
 
-const App = () => <Home />;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/sobre">
+          <>
+          </>
+        </Route>
+        <Route path="/contato">
+          <>
+          </>
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
 
 export default App;
